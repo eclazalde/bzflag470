0c893daca9effcb0444481949a712c1ef7c7604a
Devon <development@devonck.com>
2015-05-12 21:48:27 -0600

    More updates to the field visualization
    Options to setup hard-coded maps; switch between goal and home base

--- client/FieldClass.py
+++ client/FieldClass.py
@@ -25,6 +25,9 @@ class Field:
     spreadRepulsive = 1
     spreadTangential = 1
     
+    home = [0,0]
+    homeIsGoal = False
+    
     def __init__(self, minX, maxX, minY, maxY):
         print "Field initializing ..."
         self.wAttractive = 1
@@ -43,6 +46,8 @@ class Field:
         self.spreadRepulsive = 1
         self.spreadTangential = 1
         
+        self.homeIsGoal = False
+        
         # setup matrices
         self.clearFields()
         self.clearGoals()
@@ -51,12 +56,17 @@ class Field:
     def clearFields(self):
         self.a_x = np.zeros((self.range_x, self.range_y))
         self.a_y = np.zeros((self.range_x, self.range_y))
+        self.h_x = np.zeros((self.range_x, self.range_y))
+        self.h_y = np.zeros((self.range_x, self.range_y))
         self.r_x = np.zeros((self.range_x, self.range_y))
         self.r_y = np.zeros((self.range_x, self.range_y))
         self.t_x = np.zeros((self.range_x, self.range_y))
         self.t_y = np.zeros((self.range_x, self.range_y))
         self.potential_x = np.zeros((self.range_x, self.range_y))
         self.potential_y = np.zeros((self.range_x, self.range_y))
+        
+    def getMaxAttraction(self):
+        return (self.wAttractive * self.spreadAttractive)
     
     def setAttractionWeight(self, weight):
         self.wAttractive = weight
@@ -95,16 +105,61 @@ class Field:
         self.goals.append([goalX, goalY])
         # print self.goals
         
-    def addObstacle(self, obstacleX, obstacleY):
-        # print "TO-DO: addObstacle"
-
-        self.obstacles.append([obstacleX, obstacleY])
-        # print "Obstacle added"
+    def setHomeBase(self, homeX, homeY):
+        self.home = [homeX, homeY]
         
+    def switchToHome(self, goalIsHome):
+        self.homeIsGoal = goalIsHome
+        if (self.homeIsGoal):
+            self.potential_x = self.h_x + self.r_x + self.t_x
+            self.potential_y = self.h_y + self.r_y + self.t_y
+        else:
+            self.potential_x = self.a_x + self.r_x + self.t_x
+            self.potential_y = self.a_y + self.r_y + self.t_y
+        
+    def addObstacle(self, obstacleX, obstacleY):
+        if ([obstacleX, obstacleY] not in self.obstacles):
+            self.obstacles.append([obstacleX, obstacleY])
+            # print "Obstacle added"
+        
+    def setupMap4Ls(self):
+        self.clearFields()
+        self.clearGoals()
+        self.clearObstacles()
+        self.addObstacle(150, 150); self.addObstacle(150, 90); self.addObstacle(90, 90); self.addObstacle(90.0, 150.0)
+        self.addObstacle(150.0,210.0); self.addObstacle(150.0,150.0); self.addObstacle(90.0, 150.0); self.addObstacle(90.0, 210.0)
+        self.addObstacle(210.0, 150.0); self.addObstacle(210.0, 90.0); self.addObstacle(150.0, 90.0); self.addObstacle(150.0, 150.0)
+        self.addObstacle(150.0, -90.0); self.addObstacle(150.0, -150.0); self.addObstacle(90.0, -150.0); self.addObstacle(90.0, -90.0)
+        self.addObstacle(210.0, -90.0); self.addObstacle(210.0, -150.0); self.addObstacle(150.0, -150.0); self.addObstacle(150.0, -90.0)
+        self.addObstacle(150.0, -150.0); self.addObstacle(150.0, -210.0); self.addObstacle(90.0, -210.0); self.addObstacle(90.0, -150.0)
+        self.addObstacle(-90.0, -90.0); self.addObstacle(-90.0, -150.0); self.addObstacle(-150.0, -150.0); self.addObstacle(-150.0, -90.0)
+        self.addObstacle(-90.0, -150.0); self.addObstacle(-90.0, -210.0); self.addObstacle(-150.0, -210.0); self.addObstacle(-150.0, -150.0)
+        self.addObstacle(-150.0, -90.0); self.addObstacle(-150.0, -150.0); self.addObstacle(-210.0, -150.0); self.addObstacle(-210.0, -90.0)
+        self.addObstacle(-90.0, 150.0); self.addObstacle(-90.0, 90.0); self.addObstacle(-150.0, 90.0); self.addObstacle(-150.0, 150.0)
+        self.addObstacle(-90.0, 210.0); self.addObstacle(-90.0, 150.0); self.addObstacle(-150.0, 150.0); self.addObstacle(-150.0, 210.0)
+        self.addObstacle(-150.0, 150.0); self.addObstacle(-150.0, 90.0); self.addObstacle(-210.0, 90.0); self.addObstacle(-210.0, 150.0)
+        self.addObstacle(10.0,60.0);self.addObstacle(10.0,-60.0);self.addObstacle(-10.0, -60.0);self.addObstacle(-10.0, 60.0)
+        self.addObstacle(10, 20); self.addObstacle(10, -20); self.addObstacle(-10.0, -20.0);self.addObstacle(-10.0, 20.0)
+        self.addGoal(370,0)
+        self.setHomeBase(-370, 0)
+        
+    def setupMapRotatedBoxes(self):
+        self.clearFields()
+        self.clearGoals()
+        self.clearObstacles()
+        self.addObstacle(100.0, 42.4264068712); self.addObstacle(142.426406871, 0.0); self.addObstacle(100.0, -42.4264068712); self.addObstacle(57.5735931288, 0)
+        self.addObstacle(-100.0, 42.4264068712); self.addObstacle(-57.5735931288, 0.0); self.addObstacle(-100.0, -42.4264068712); self.addObstacle(-142.426406871, 0)
+        self.addObstacle(0, 142.426406871); self.addObstacle(42.4264068712, 100.0); self.addObstacle(0, 57.5735931288); self.addObstacle(-42.4264068712, 100.0)
+        self.addObstacle(0, -57.5735931288); self.addObstacle(42.4264068712, -100.0); self.addObstacle(0, -142.426406871); self.addObstacle(-42.4264068712, -100.0);
+        self.addGoal(-370,0)
+        self.setHomeBase(370, 0)
+
     def distance(self, p1, p2):
         return np.sqrt( (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 )
         
     def calculateFields(self):
+        maxrepx = 0
+        maxrepy = 0
         print "Calculating fields ..."
         for i in range(self.range_x):
             for j in range(self.range_y):
@@ -128,14 +183,28 @@ class Field:
                     elif (dist > (self.radiusAttractive + self.spreadAttractive)):
                         self.a_x[j][i] += self.wAttractive * self.spreadAttractive * np.cos(theta)
                         self.a_y[j][i] += self.wAttractive * self.spreadAttractive * np.sin(theta)
+                
+                # calculate for home base
+                dist = self.distance((xPos, yPos), self.home)
+                theta = np.arctan2(self.home[1] - yPos, self.home[0] - xPos) 
+                if (dist < self.radiusAttractive):
+                    self.h_x[j][i] += 0
+                    self.h_y[j][i] += 0
+                elif ((self.radiusAttractive <= dist) and (dist <= (self.radiusAttractive + self.spreadAttractive))):
+                    self.h_x[j][i] += self.wAttractive * ( (dist - self.radiusAttractive)*np.cos(theta) )
+                    self.h_y[j][i] += self.wAttractive * ( (dist - self.radiusAttractive)*np.sin(theta) )
+                elif (dist > (self.radiusAttractive + self.spreadAttractive)):
+                    self.h_x[j][i] += self.wAttractive * self.spreadAttractive * np.cos(theta)
+                    self.h_y[j][i] += self.wAttractive * self.spreadAttractive * np.sin(theta)   
+                
                 for o in self.obstacles:
                     dist = self.distance((xPos, yPos), o)
                     theta = np.arctan2(o[1] - yPos, o[0] - xPos)
                     repx = 0
                     repy = 0
                     if (dist < self.radiusRepulsive):
-                        repx = (-np.sign(np.cos(theta)) * 999) # set to zero? tank cannot be inside
-                        repy = (-np.sign(np.sin(theta)) * 999) # set to zero? tank cannot be inside
+                        repx = (-np.sign(np.cos(theta)) * 1) # set to zero? tank cannot be inside
+                        repy = (-np.sign(np.sin(theta)) * 1) # set to zero? tank cannot be inside
                     elif ((self.radiusRepulsive <= dist) and (dist <= (self.radiusRepulsive + self.spreadRepulsive))):
                         repx = -self.wRepulsive * ( (self.spreadRepulsive + self.radiusRepulsive - dist) * np.cos(theta))
                         repy = -self.wRepulsive * ( (self.spreadRepulsive + self.radiusRepulsive - dist) * np.sin(theta))
@@ -144,6 +213,13 @@ class Field:
                         repy = 0
                     self.r_x[j][i] += repx
                     self.r_y[j][i] += repy
+                    
+                    #if (np.abs(repx) > maxrepx):
+                    #    maxrepx = np.abs(repx)
+                    #    print 'new max X at %s, %s of %s' % (self.x_min+i,self.y_min+j,maxrepx)
+                    #if (np.abs(repy) > maxrepy):
+                    #    maxrepy = np.abs(repy)
+                    #    print 'new max Y at %s, %s of %s' % (self.x_min+i,self.y_min+j,maxrepy)
                     # calculate tangents
                     
                     clockwise = theta - (np.pi / 2)
@@ -166,12 +242,29 @@ class Field:
         # print x, y
         return [self.potential_x[y][x], self.potential_y[y][x]]
     
-    def visualize(self):
+    def visualize(self, attractive, repulsive, tangential):
         print "Preparing viz export ..."
+        
+        viz_x = np.zeros((self.range_x, self.range_y))
+        viz_y = np.zeros((self.range_x, self.range_y))
+        
+        if (attractive):
+            viz_x += self.a_x
+            viz_y += self.a_y
+        if (repulsive):
+            viz_x += self.r_x
+            viz_y += self.r_y
+        if (tangential):
+            viz_x += self.t_x
+            viz_y += self.t_y
         
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        segments = 25
+        
+        segments = 50
+        spacebetween = (self.x_max - self.x_min) / segments
+        
+        starting = spacebetween / 2
         x = np.linspace(self.x_min, self.x_max, segments)
         y = np.linspace(self.y_min, self.y_max, segments)
         x,y = np.meshgrid(x,y)
@@ -179,10 +272,15 @@ class Field:
         sampled_y = np.zeros(np.shape(y))
         for i in range(0,segments,1):
             for j in range(0,segments,1):
-                sampled_x[j][i] = self.potential_x[16 + j * 32][16 + i * 32]
-                sampled_y[j][i] = self.potential_y[16 + j * 32][16 + i * 32]
+                sampled_x[j][i] = viz_x[starting + j * spacebetween][starting + i * spacebetween]
+                sampled_y[j][i] = viz_y[starting + j * spacebetween][starting + i * spacebetween]
         # print np.sum(sampled_x)
-        ax.quiver(x, y, sampled_x, sampled_y, pivot='middle', color='r')
+        ax.quiver(x, y, sampled_x, sampled_y, pivot='mid', color='r', headlength=6, headwidth=4, units='inches')
+        '''
+        x = np.linspace(self.x_min, self.x_max)
+        y = np.linspace(self.y_min, self.y_max)
+        ax.quiver(x[::10, ::10], y[::10, ::10], viz_x[::10, ::10], viz_y[::10, ::10], pivot='mid', color='r')
+        '''
         for g in self.goals:
             fig.gca().add_artist(plt.Circle(g, radius=self.radiusAttractive, color='g', fill=False))
         for o in self.obstacles:
--- client/FieldTest.py
+++ client/FieldTest.py
@@ -7,20 +7,28 @@ f.setAttractionRadius(5)
 f.setAttractionSpread(1132)
 
 f.setRepulsionWeight(.5)
-f.setRepulsionRadius(10)
-f.setRepulsionSpread(100)
+f.setRepulsionRadius(2)
+f.setRepulsionSpread(50)
 
 f.setTangentialWeight(2)
 f.setTangentialRadius(10)
-f.setTangentialSpread(30)
+f.setTangentialSpread(12)
 
+'''
 f.clearGoals()
 f.clearObstacles()
-f.addGoal(380,0)
+f.addGoal(370,0) # green flag
 f.addObstacle(-100,-100)
+'''
+
+#f.setupMap4Ls()
+f.setupMapRotatedBoxes()
+
 f.calculateFields()
 
-print f.queryPosition(0, 100)
+#print f.queryPosition(0, 100)
 
-f.visualize()
-
+f.visualize(True, True, True) # all fields
+#f.visualize(True, False, False) # attractive only
+#f.visualize(False, True, False) # repulsive only
+#f.visualize(False, False, True) # tangential only
