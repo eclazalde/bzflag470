dd56895ece918a055709d3c2726eaceed2201179
Devon <development@devonck.com>
2015-06-10 22:28:10 -0600

    Minor refactoring changes to loadFile

--- client/lab4/UtilityFunctions.py
+++ client/lab4/UtilityFunctions.py
@@ -14,29 +14,33 @@ class UtilityFunctions:
         ''' Set the path of the file to parse '''
         self._filepath = FilePath
     
-    def loadFile(self):
+    def loadFile(self, getTags=False):
         ''' Loads the file into memory and builds an n-gram dictionary '''
-        self._nGramDict = dict()
-        f = open(self._filepath, 'r')
-        for f_line in f:
-            f_line_parts = split(f_line, sep=None)
-            w_previous = None
-            for w in f_line_parts:
-                w_parts = split(w, sep='_')
-                w_next = w_parts[0]
-                if (w_previous): # skip the first word
-                    if w_previous in self._nGramDict:
-                        d = self._nGramDict.get(w_previous)
-                        v = 0 # number of occurrences
-                        if w_next in d:
-                            v = d.get(w_next)
-                        v += 1
-                        d[w_next] = v
-                    else:
-                        d = dict([(w_next, 1)])
-                        self._nGramDict[w_previous] = d  
-                w_previous = w_next
-                
+        if (getTags):
+            print "NOT YET IMPLEMENTED"
+            return
+        else:
+            self._nGramDict = dict()
+            f = open(self._filepath, 'r')
+            for f_line in f:
+                f_line_parts = split(f_line, sep=None)
+                w_previous = None
+                for w in f_line_parts:
+                    w_parts = split(w, sep='_')
+                    w_next = w_parts[0]
+                    if (w_previous): # skip the first word
+                        if w_previous in self._nGramDict:
+                            d = self._nGramDict.get(w_previous)
+                            v = 0 # number of occurrences
+                            if w_next in d:
+                                v = d.get(w_next)
+                            v += 1
+                            d[w_next] = v
+                        else:
+                            d = dict([(w_next, 1)])
+                            self._nGramDict[w_previous] = d  
+                    w_previous = w_next  
+                      
     def getNGramDictionary(self):
         ''' Returns the full n-gram dictionary object '''
         return self._nGramDict
--- client/lab4/testUtilityFunctions.py
+++ client/lab4/testUtilityFunctions.py
@@ -3,7 +3,7 @@ import time
 
 uf = UtilityFunctions()
 start = time.time()
-uf.loadFile()
+uf.loadFile(getTags=False)
 #print uf.getNGramDictionary()
 uf.calculateNGramProbabilities()
 #print uf.getNGramDictionary()
@@ -11,7 +11,7 @@ print (time.time() - start), "second(s) to load", uf.getFilePath()
 
 start = time.time()
 uf.setFilePath("./training_dataset.txt")
-uf.loadFile()
+uf.loadFile(getTags=False)
 #print uf.getNGramDictionary()
 uf.calculateNGramProbabilities()
 #print uf.getNGramDictionary()
